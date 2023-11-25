d310785a42891a5559b9a3678118a25b7d6ddbaa
mayank5654 <mayankkaushal99@gmail.com>
2023-11-25 23:12:27 +0530

    intial commit

---
+++ practice project-1.14-retrieve student data/practice project-1.14-retrieve student data/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace practice_project_1._14_retrieve_student_data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------RAINBOW SCHOOL---------------------");
+
+
+            Console.WriteLine("                                                         ");
+            Console.WriteLine("---------------------STUDENTS DATA----------------------");
+
+            try
+            {
+                string fpath = "D:/practic exercise/practice project/project1.14/student data.txt";
+                string[] lines = File.ReadAllLines(fpath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+               
+            }
+            catch (Exception ex)
+            { Console.WriteLine("error!!!" + ex.Message); }
+            finally { Console.ReadKey(); }
+
+        }
+
+    }
+}
+
